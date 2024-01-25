ab0bb935d80a82edfa83cc5642a3db168f028e10
Ante-237 <akumawahanye@gmail.com>
2024-01-26 01:19:08 +0200

    main ui

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BuyTime',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+        useMaterial3: false,
       ),
-      home: const MyHomePage(title: 'Home'),
+      home: const MyHomePage(title: 'BuyTime'),
     );
   }
 }
@@ -37,41 +37,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                    text: "gigs",
+                    icon: Icon(Icons.work),
+
+                ),
+                Tab(
+                  text: "Create",
+                    icon: Icon(Icons.post_add)
+                ),
+                Tab(
+                  text: "chats",
+                    icon: Icon(Icons.settings),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Text('New World', textDirection: TextDirection.ltr, style: TextStyle(fontSize: 32, color: Colors.blue),),
-          ],
+            title: const Text('Tabs Demo'),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.work),
+              Icon(Icons.post_add_outlined),
+              Icon(Icons.settings_outlined),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
